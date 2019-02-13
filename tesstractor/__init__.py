62ddcf192ba5cf8f2df9e02f880906d7728e7511
Sergio Pascual <sergiopr@fis.ucm.es>
2019-02-13 10:54:40 +0100

    Add __version__ to top devel module

---
+++ tesstractor/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright 2018-2019 Universidad Complutense de Madrid
+#
+# This file is part of tesstractor
+#
+# SPDX-License-Identifier: GPL-3.0+
+# License-Filename: LICENSE.txt
+#
+
+__version__ = '0.6.dev0'
